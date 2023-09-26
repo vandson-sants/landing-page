8f9cacd33b05a999cc6111599e82506b3e104e18
Vandson Santos <vandson.okedoke@gmail.com>
2023-09-26 15:30:09 -0300

    animação e botão de topo

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-//adicionar rolagem topo
+//rolagem ao topo
+window.addEventListener("scroll", function () {
+  let scroll = document.querySelector(".btn-scrollTop");
+  scroll.classList.toggle("active", window.scrollY > 450);
+});
+
+function backTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
